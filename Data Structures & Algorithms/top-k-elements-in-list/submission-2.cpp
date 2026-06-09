f9ca06ea0ab6a302027c17a9db6d246635209f15
adityafrfr <mail4adityaa@gmail.com>
2026-06-09 14:34:41 +0530

    Add: top-k-elements-in-list - submission-2

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-2.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        unordered_map<int, int> hmap;
+
+        for (int num : nums)    {
+            hmap[num]++;
+        }   
+
+        vector<vector<int>> buckets(nums.size() + 1);
+
+        for (auto &it:hmap) {
+            buckets[it.second].push_back(it.first);
+        }
+
+        vector<int> soln;
+
+        for (int i = buckets.size() - 1; i >= 0; --i)   {
+            if (buckets[i].empty()) continue;
+
+            for (int num : buckets[i])  {
+                if (soln.size() == k) return soln;
+
+                soln.push_back(num);
+            }
+        }
+
+        return soln;
+        
+    }
+};
